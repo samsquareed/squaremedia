d9ee02fe28e626203dba633c1f2933758c679c71
samsquareed <samsquare.professional@gmail.com>
2022-04-25 11:48:53 +0530

    login and signup components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 import Profile from './pages/profile/Profile'
+import Register from './pages/register/Register'
 
 const App = () => {
   return (
-    <Profile />
+    <Register />
   )
 }
 
