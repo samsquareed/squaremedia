2f30e4df85d9af6e6a1e240aa544c0d7678146f3
samsquareed <samsquare.professional@gmail.com>
2022-04-25 10:34:47 +0530

    amazing post component is completed

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,37 @@
+import "./post.css"
+import { MoreVert } from "@material-ui/icons";
+
+const Post = () => {
+  return (
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img className="postProfileImg" src="./assets/person/1.jpeg" alt="" />
+                    <span className="postUsername">Sammed</span>
+                    <span className="postDate">9 min ago</span>
+                </div>
+                <div className="postTopRight">
+                    <MoreVert />
+                </div>
+            </div>
+            <div className="postCenter">
+            <span className="postText"> social media developed by sammed</span>
+          <img className="postImg" src= "./assets/post/7.jpeg" alt="" />
+            </div>
+            <div className="postBottom">
+            <div className="postBottomLeft">
+                <img className="likeIcon" src="assets/like.png" alt="" />
+                <img className="likeIcon" src="assets/heart.png" alt="" />
+                <span className="postLikeCounter">9M people like it</span>
+            </div>
+            <div className="postBottomRight">
+                <span className="postCommentText"> 1M comments</span>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Post
